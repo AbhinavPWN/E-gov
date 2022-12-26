3b717af1ddffe78960afd04052bb8569b614c5bc
AbhinavPWN <104517482+AbhinavPWN@users.noreply.github.com>
2022-12-26 21:50:04 +0545

    newcommit

--- Home/forms.py
+++ Home/forms.py
@@ -2,70 +2,83 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 from .models import Idcard
-from django import forms 
+from django import forms
 
-CHOICES = [('1', '-----'), ('2', 'Kathmandu'),
-           ('3', 'Bhaktapur'), ('4', 'Lalitpur'), ('5', 'Chitwan')]
-gen = [('1', '-----'), ('2', 'Female'),
-       ('3', 'Male'), ('4', 'Others')]
-stat = [('1', '-------'), ('2', 'Single'),
-        ('3', 'Divorce'),('4','Married')]
-Edu_level = [('1', '------'), ('2', 'Slc'),('3','Plus 2'),
-             ('4', 'Bacehlors'), ('5', 'Masters'), ('6', 'PHDs'), ('7', 'Uneducated')]
-prof = [('1', '-----'), ('2', 'Service'), ('3', 'Doctor'), ('4', 'Framer'),
-        ('5', 'Teacher'), ('6', 'Social Worker'), ('7', 'Lawyer'), ('8', 'Businessman'), ('9', 'Others')]
-CasteChoice = [('1', '----'), ('2', 'Chettri'), ('3', 'Brahmin'), ('4', 'Newar'),
-               ('5', 'Thakuri'), ('6', 'Others')]
-Reli = [('1', '------'), ('2', 'Hindu'), ('3', 'Bouddha'), ('4', 'Islam'),
-        ('5', 'Christian'), ('6', 'Others')]
+CHOICES = [
+    ('Chitwan', 'Chitwan'), ('Kathmandu', 'Kathmandu'),
+    ('Bhaktapur', 'Bhaktapur'), ('Lalitpur', 'Lalitpur'), ]
+gen = [('Male', 'Male'), ('Female', 'Female'),
+       ('Others', 'Others'), ]
+stat = [('Married', 'Married'), ('Single', 'Single'),
+        ('Divorce', 'Divorce'), ]
+Edu_level = [('Uneducated', 'Uneducated'), ('Slc', 'Slc'), ('Plus_2', 'Plus 2'),
+             ('Bachelors', 'Bachelors'), ('Masters', 'Masters'), ('PHDs', 'PHDs'), ]
+prof = [('Others', 'Others'), ('Service', 'Service'), ('Doctor', 'Doctor'), ('Framer', 'Framer'),
+        ('Teacher', 'Teacher'), ('Social', 'Social Worker'), ('Lawyer', 'Lawyer'), ('Businessman', 'Businessman')]
+CasteChoice = [('Others', 'Others'), ('Chettri', 'Chettri'), ('Brahmin', 'Brahmin'), ('Newar', 'Newar'),
+               ('Thakuri', 'Thakuri')]
+Reli = [('Others', 'Others'), ('Hindu', 'Hindu'), ('Buddha', 'Buddha'), ('Islam', 'Islam'),
+        ('Christian', 'Christian')]
 
 
 class card(forms.ModelForm):
-    firstname =forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','Placeholder':'Enter Firstname'}))
-    middlename =forms.CharField(required=False,widget=forms.TextInput(attrs={'class':'form-control my-2','Placeholder':'Enter midlename'}))
-    lastname =forms.CharField(widget=forms.TextInput(attrs={'class':'form-control my-2','Placeholder':'Enter lastname'}))
-    dob = forms.DateField(widget=forms.NumberInput(attrs={'class': 'form-control my-2', 'type': 'date','Placeholder': 'Enter Date'}))
+    firstname = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control my-2', 'Placeholder': 'Enter Firstname'}))
+    middlename = forms.CharField(required=False, widget=forms.TextInput(
+        attrs={'class': 'form-control my-2', 'Placeholder': 'Enter middlename'}))
+    lastname = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control my-2', 'Placeholder': 'Enter lastname'}))
+    dob = forms.DateField(widget=forms.NumberInput(
+        attrs={'class': 'form-control my-2', 'type': 'date', 'Placeholder': 'Enter Date'}))
     birthplace = forms.ChoiceField(choices=CHOICES, widget=forms.Select(
         attrs={'class': 'form-control my-2', 'Placeholder': 'Choose Birthplace'}))
     citizenship_no = forms.IntegerField(widget=forms.NumberInput(
         attrs={'class': 'form-control my-2', 'Placeholder': 'Enter Citizenship Number'}))
-    issue_zone = forms.ChoiceField(label='Zone', choices=CHOICES,widget=forms.Select(
+    issue_zone = forms.ChoiceField(label='Zone', choices=CHOICES, widget=forms.Select(
         attrs={'class': 'form-control my-2', 'Placeholder': 'enter zone'}))
     # issue_date = forms.DateField(widget=forms.DateInput(
     #     attrs={'class': 'form-control my-2', 'Placeholder': 'enter issue_date'}))
-    
+
     gender = forms.ChoiceField(label='Gender',  choices=gen, widget=forms.Select(
         attrs={'class': 'form-control my-2', 'Placeholder': 'Choose gender'}))
-    
-    marital_status = forms.ChoiceField(choices=stat ,widget=forms.Select(
+
+    marital_status = forms.ChoiceField(choices=stat, widget=forms.Select(
         attrs={'class': 'form-control my-2', 'Placeholder': 'Choose Marital State'}))
-    
-    education = forms.ChoiceField(choices=Edu_level,widget=forms.Select(
+
+    education = forms.ChoiceField(choices=Edu_level, widget=forms.Select(
         attrs={'class': 'form-control my-2', 'Placeholder': 'Choose Education level'}))
-    
-    profession = forms.ChoiceField(choices=prof,widget=forms.Select(
+
+    profession = forms.ChoiceField(choices=prof, widget=forms.Select(
         attrs={'class': 'form-control my-2', 'Placeholder': 'Choose Profession'}))
-    
-    caste = forms.ChoiceField(choices=CasteChoice,widget=forms.Select(
+
+    caste = forms.ChoiceField(choices=CasteChoice, widget=forms.Select(
         attrs={'class': 'form-control my-2', 'Placeholder': 'Choose Caste'}))
-    
-    religion= forms.ChoiceField(choices=Reli, widget=forms.Select(
+
+    religion = forms.ChoiceField(choices=Reli, widget=forms.Select(
         attrs={'class': 'form-control my-2', 'Placeholder': 'Choose Caste'}))
-    
+
+    # def clean_dob(self):
+    #     data = self.cleaned_data.get["dob"]
+    #     if data == '':
+    #         raise forms.ValidationError('Enter the date')
+    #     return data
+
     class Meta:
-        model= Idcard
-        fields=['firstname','middlename','lastname','dob','birthplace','citizenship_no','issue_zone','gender','marital_status','education','profession','caste','religion']
-        
+        model = Idcard
+        fields = ['firstname', 'middlename', 'lastname', 'dob', 'birthplace', 'citizenship_no',
+                  'issue_zone', 'gender', 'marital_status', 'education', 'profession', 'caste', 'religion']
+
 
 class CustomUserForm(UserCreationForm):
-    username= forms.CharField(widget=forms.TextInput(
-        attrs={'class':'form-control my-2','placeholder':'Enter Username'}))
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control my-2', 'placeholder': 'Enter Username'}))
     email = forms.CharField(widget=forms.EmailInput(
         attrs={'class': 'form-control my-2', 'placeholder': 'Enter Email'}))
     password1 = forms.CharField(widget=forms.PasswordInput(
         attrs={'class': 'form-control my-2', 'placeholder': 'Enter Password'}))
     password2 = forms.CharField(widget=forms.PasswordInput(
         attrs={'class': 'form-control my-2', 'placeholder': 'Confirm Password'}))
+
     class Meta:
-        model= User
-        fields =['username','email','password1','password2']
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- Home/models.py
+++ Home/models.py
@@ -3,32 +3,34 @@ from django.contrib.auth.models import User
 # from django.contrib.auth.models import card
 
 
-
-CHOICES=[
-    ('1', 'Chitwan'), ('2', 'Kathmandu'),
-       ('3', 'Bhaktapur'), ('4', 'Lalitpur'),]
-gen = [('1', 'Male'), ('2', 'Female'),
-       ('3', 'Others'), ]
-stat = [('1', 'Married'), ('2', 'Single'),
-        ('3', 'Divorce'), ]
-Edu_level = [('1', 'Uneducated'), ('2', 'Slc'), ('3', 'Plus 2'),
-             ('4', 'Bacehlors'), ('5', 'Masters'), ('6', 'PHDs'), ]
-prof = [('1', 'Others'), ('2', 'Service'), ('3', 'Doctor'), ('4', 'Framer'),
-        ('5', 'Teacher'), ('6', 'Social Worker'), ('7', 'Lawyer'), ('8', 'Businessman')]
-CasteChoice = [('1', 'Others'), ('2', 'Chettri'), ('3', 'Brahmin'), ('4', 'Newar'),
-               ('5', 'Thakuri')]
-Reli = [('1', 'Others'), ('2', 'Hindu'), ('3', 'Bouddha'), ('4', 'Islam'),
-        ('5', 'Christian')]
+CHOICES = [
+    ('Chitwan', 'Chitwan'), ('Kathmandu', 'Kathmandu'),
+    ('Bhaktapur', 'Bhaktapur'), ('Lalitpur', 'Lalitpur'), ]
+gen = [('Male', 'Male'), ('Female', 'Female'),
+       ('Others', 'Others'), ]
+stat = [('Married', 'Married'), ('Single', 'Single'),
+        ('Divorce', 'Divorce'), ]
+Edu_level = [('Uneducated', 'Uneducated'), ('Slc', 'Slc'), ('Plus_2', 'Plus 2'),
+             ('Bacehlors', 'Bacehlors'), ('Masters', 'Masters'), ('PHDs', 'PHDs'), ]
+prof = [('Others', 'Others'), ('Service', 'Service'), ('Doctor', 'Doctor'), ('Framer', 'Framer'),
+        ('Teacher', 'Teacher'), ('Social', 'Social Worker'), ('Lawyer', 'Lawyer'), ('Businessman', 'Businessman')]
+CasteChoice = [('Others', 'Others'), ('Chettri', 'Chettri'), ('Brahmin', 'Brahmin'), ('Newar', 'Newar'),
+               ('Thakuri', 'Thakuri')]
+Reli = [('Others', 'Others'), ('Hindu', 'Hindu'), ('Buddha', 'Buddha'), ('Islam', 'Islam'),
+        ('Christian', 'Christian')]
 # Create your models here.
+
+
 class FilesAdmin(models.Model):
     adminupload = models.FileField(upload_to='uploads/')
-    title= models.CharField(max_length=50)
-    
+    title = models.CharField(max_length=50)
+
     class Meta:
         verbose_name_plural = 'Files Uploads'
-        
+
     def __str__(self):
         return self.title
+
 
 class Idcard(models.Model):
     firstname = models.CharField(max_length=50)
@@ -37,27 +39,27 @@ class Idcard(models.Model):
     dob = models.DateField(auto_now=False, auto_now_add=False)
     birthplace = models.CharField(choices=CHOICES, max_length=50)
     citizenship_no = models.IntegerField()
-    issue_zone = models.CharField( max_length=50)
+    issue_zone = models.CharField(max_length=50)
     gender = models.CharField(max_length=50)
     marital_status = models.CharField(max_length=50)
     education = models.CharField(max_length=50)
     profession = models.CharField(max_length=50)
     caste = models.CharField(max_length=50)
     religion = models.CharField(max_length=50)
-    
+
     class Meta:
         verbose_name_plural = 'Id Cards'
-        
+
     def __str__(self):
         return self.firstname
 
+
 class Gallery(models.Model):
-        title = models.CharField(max_length=150)
-        image =models.ImageField(upload_to='uploads', null=True, blank=True )
-        
-        class Meta:
-                verbose_name_plural='Galleries'
-                
-        def __str__(self):
-            return self.title
-        
+    title = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='uploads', null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Galleries'
+
+    def __str__(self):
+        return self.title
